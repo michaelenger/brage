ffd99bf5f5b2149546d0921dce6aa30d388a1378
Michael Enger <github@michaelenger.com>
2022-01-02 13:02:45 +0100

    Change site init to use a map

--- cmd/init.go
+++ cmd/init.go
@@ -9,54 +9,6 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var configContent = `title: My Site
-description: This is my Brage site.
-image: dog.png
-rootUrl: https://example.org
-
-data:
-  words:
-    - banana
-    - happy
-    - explosion
-`
-
-var layoutContent = `<!DOCTYPE html>
-<html>
-<head>
-<link rel="stylesheet" type="text/css" href="/assets/style.css">
-<title>{{ .Site.Title }}</title>
-</head>
-
-<body>
-{{ .Content }}
-</body>
-</html>
-`
-
-var styleContent = `body {
-	background: #eee;
-	color: #222;
-	font-family: Helvetica, sans-serif;
-}`
-
-var indexContent = `<p>This is the main page.</p>
-{{ template "extra" . }}
-<a href="/about">About</a>
-`
-
-var aboutContent = `<p>This is the about page.</p>
-{{ template "extra" . }}
-<a href="/">Home</a>
-`
-
-var extraContent = `<ul>
-{{ range .Data.words }}
-<li>{{ . }}</li>
-{{ end }}
-</ul>
-`
-
 var initCmd = &cobra.Command{
 	Use:   "init [PATH]",
 	Short: "Initialize a new blank site",
@@ -79,82 +31,65 @@ var initCmd = &cobra.Command{
 
 		logger.Printf("Creating site in: %v", targetPath)
 
-		if err := os.MkdirAll(targetPath, 0755); err != nil {
-			logger.Fatalf("ERROR! Unable to create target directory: %v", err)
+		files := map[string]string {
+			"config.yaml": `title: My Site
+description: This is my Brage site.
+image: dog.png
+rootUrl: https://example.org
+
+data:
+  words:
+    - banana
+    - happy
+    - explosion
+`,
+			"layout.html": `<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="/assets/style.css">
+<title>{{ .Site.Title }}</title>
+</head>
+
+<body>
+<h1>{{ .Site.Title }}</h1>
+{{ .Content }}
+</body>
+</html>
+`,
+			"assets/style.css": `body {
+	background: #eee;
+	color: #222;
+	font-family: Helvetica, sans-serif;
+}`,
+			"pages/index.html": `<p>This is the main page.</p>
+{{ template "extra" . }}
+<a href="/about">About</a>
+`,
+			"pages/about.markdown": `This is the about page.
+
+[Home](/)
+`,
+			"templates/extra.html": `<ul>
+{{ range .Data.words }}
+<li>{{ . }}</li>
+{{ end }}
+</ul>
+`,
 		}
 
-		// Config
+		for filePath, contents := range files {
+			fullPath := path.Join(targetPath, filePath)
 
-		configFile := path.Join(targetPath, "config.yaml")
-		if err := utils.WriteFile(configFile, configContent); err != nil {
-			logger.Fatalf("ERROR! Unable to create config file: %v", err)
+			if err := os.MkdirAll(path.Dir(fullPath), 0755); err != nil {
+				logger.Fatalf("ERROR! Unable to create directory: %v", err)
+			}
+
+			if err := utils.WriteFile(fullPath, contents); err != nil {
+				logger.Fatalf("ERROR! Unable to create file: %v", err)
+			}
+
+			logger.Printf("Created: %v", filePath)
 		}
-
-		logger.Print("Created: /config.yaml")
-
-		// Layout
-
-		layoutFile := path.Join(targetPath, "layout.html")
-		if err := utils.WriteFile(layoutFile, layoutContent); err != nil {
-			logger.Fatalf("ERROR! Unable to create layout file: %v", err)
-		}
-
-		logger.Print("Created: /layout.html")
-
-		// Assets
-
-		assetsDirectory := path.Join(targetPath, "assets")
-		if err := os.Mkdir(assetsDirectory, 0755); err != nil {
-			logger.Fatalf("ERROR! Unable to create assets directory: %v", err)
-		}
-
-		logger.Print("Created: /assets")
-
-		styleFile := path.Join(assetsDirectory, "style.css")
-		if err := utils.WriteFile(styleFile, styleContent); err != nil {
-			logger.Fatalf("ERROR! Unable to create style file: %v", err)
-		}
-
-		logger.Print("Created: /assets/style.css")
-
-		// Pages
-
-		pagesDirectory := path.Join(targetPath, "pages")
-		if err := os.Mkdir(pagesDirectory, 0755); err != nil {
-			logger.Fatalf("ERROR! Unable to create pages directory: %v", err)
-		}
-
-		logger.Print("Created: /pages")
-
-		indexFile := path.Join(pagesDirectory, "index.html")
-		if err := utils.WriteFile(indexFile, indexContent); err != nil {
-			logger.Fatalf("ERROR! Unable to create index file: %v", err)
-		}
-
-		logger.Print("Created: /pages/index.html")
-
-		aboutFile := path.Join(pagesDirectory, "about.html")
-		if err := utils.WriteFile(aboutFile, aboutContent); err != nil {
-			logger.Fatalf("ERROR! Unable to create about file: %v", err)
-		}
-
-		logger.Print("Created: /pages/about.html")
-
-		// Templates
-
-		templatesDirectory := path.Join(targetPath, "templates")
-		if err := os.Mkdir(templatesDirectory, 0755); err != nil {
-			logger.Fatalf("ERROR! Unable to create templates directory: %v", err)
-		}
-
-		logger.Print("Created: /templates")
-
-		extraFile := path.Join(templatesDirectory, "extra.html")
-		if err := utils.WriteFile(extraFile, extraContent); err != nil {
-			logger.Fatalf("ERROR! Unable to create extra file: %v", err)
-		}
-
-		logger.Print("Created: /templates/extra.html")
 	},
 }
 
